d78ed1d20b204f67c22603da5257ad7f3fb4ad30
Roi Doron <roidoron6@gmail.com>
2024-02-28 12:44:27 +0200

    Add basic

---
+++ js/memeController.js
@@ -0,0 +1,43 @@
+'use strict'
+
+let gElCanvas
+let gCtx
+
+const myImage = new Image();
+myImage.src = "img/4.jpg";
+
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const lines = meme.lines
+    gCtx.drawImage(myImage, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText(lines.txt,lines.size,lines.color)
+}
+
+function drawText(txt,size,color) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = color
+
+    gCtx.fillStyle = color
+
+    gCtx.font = `${size}px Arial`
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+
+    gCtx.fillText(txt, 208, 60)
+    gCtx.strokeText(txt, 208, 60)
+}
+
+
+
+/// when there is a gallery
+// function onSelectImg(elImg) {
+//     coverCanvasWithImg(elImg)
+// }
