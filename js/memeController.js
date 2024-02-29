0394bdf22c25ec0ac06fa67719d61ee7930a1e56
Roi Doron <roidoron6@gmail.com>
2024-02-29 10:58:42 +0200

    Add delete line and click select

--- js/memeController.js
+++ js/memeController.js
@@ -4,6 +4,7 @@ let gElCanvas
 let gCtx
 let gLines = 1
 let currText = 0
+const TOUCH_EVENTS = ['touchstart', 'touchmove', 'touchend']
 const myImage = new Image();
 myImage.src = "img/4.jpg";
 
@@ -41,20 +42,22 @@ function drawText(meme) {
         gCtx.textBaseline = 'middle'
 
 
-        gCtx.fillText(line.txt, line.x, line.y)
+        gCtx.fillText(line.txt, line.pos.x, line.pos.y)
         // gCtx.strokeText(line.txt, line.x, line.y)
 
         if (meme.selectedLineIdx === i) {
             const textWidth = gCtx.measureText(line.txt).width * 1.1
             const lineHeight = line.size * 1.5
-            gCtx.strokeRect(line.x - textWidth / 2, line.y - lineHeight / 2, textWidth, lineHeight)
-            // gCtx.stroke()
+            gCtx.strokeRect(line.pos.x - textWidth / 2, line.pos.y - lineHeight / 2, textWidth, lineHeight)
+            
         }
     }
 
 }
 
 function addListeners() {
+    addMouseListeners()
+	addTouchListeners()
 
     window.addEventListener('resize', () => {
         resizeCanvas()
@@ -96,8 +99,15 @@ function onSmallerFont() {
 }
 
 function onAddLine() {
+    AddLine()
     if (gLines > 1) return
     gLines++
+    renderMeme()
+}
+
+function onDeleteLine(){
+    DeleteLine()
+    gLines--
     renderMeme()
 }
 
@@ -112,4 +122,55 @@ function onSelectLine() {
 function downloadImg(elLink) {
     const imgContent = gElCanvas.toDataURL('image/jpeg') // image/jpeg the default format
     elLink.href = imgContent
+}
+
+//event listeners
+
+function addMouseListeners() {
+	gElCanvas.addEventListener('mousedown', onDown)
+	// gElCanvas.addEventListener('mousemove', onMove)
+	// gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+	gElCanvas.addEventListener('touchstart', onDown)
+	// gElCanvas.addEventListener('touchmove', onMove)
+	// gElCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+	
+	// const clickedPos = getEvPos(ev)        
+    // console.log(isTextClick(clickedPos))
+	
+}
+
+function onCanvasClick(ev){
+    const clickedPos = getEvPos(ev)        
+    const line = isTextClick(clickedPos)
+    SelectLineWithClick(line)
+    const elInput = document.querySelector('.text')
+    elInput.value = line.txt
+    renderMeme()
+
+}
+
+function getEvPos(ev) {
+	let pos = {
+		x: ev.offsetX,
+		y: ev.offsetY,
+	}
+
+	if (TOUCH_EVENTS.includes(ev.type)) {
+		
+		ev.preventDefault()         // Prevent triggering the mouse events
+		ev = ev.changedTouches[0]   // Gets the first touch point
+
+		// Calc pos according to the touch screen
+		pos = {
+			x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+			y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+		}
+	}
+	return pos
 }
--- js/service/memeService.js
+++ js/service/memeService.js
@@ -7,17 +7,18 @@ var gMeme = {
     selectedLineIdx: 0,
     lines: [
         {
-            txt: 'I sometimes eat Falafel',
+            txt:'text',
             size: 20,
             color: 'black',
-            x: 225,
-            y: 60
+            pos:{x:225,y:60,rate :20},
+            id:0
         }, {
-            txt: 'I sometimes eat Falafel',
+            txt:'text',
             size: 20,
             color: 'black',
-            x: 225,
-            y: 380
+            pos:{x:225,y:380,rate :20},
+            id:1
+           
         }
     ]
 
@@ -29,6 +30,7 @@ function getMeme() {
 
 function setLineText(txt, line) {
     gMeme.lines[gMeme.selectedLineIdx].txt = txt
+    
 }
 
 function setImg(img) {
@@ -54,12 +56,54 @@ function SmallerFont() {
 
 function changePlaceTxt(offsetWidth,offsetHeight){
     const x =  offsetWidth/2
-    gMeme.lines.forEach(line => line.x =x)
-    gMeme.lines[1].y = offsetHeight -60
+    gMeme.lines.forEach(line => line.pos.x =x)
+    gMeme.lines[1].pos.y = offsetHeight -60
 }
 function SelectLine() {
     if (gMeme.lines.length > gMeme.selectedLineIdx + 1) gMeme.selectedLineIdx += 1
     else if (gMeme.lines.length === gMeme.selectedLineIdx + 1) gMeme.selectedLineIdx -= 1
     return gMeme.lines[gMeme.selectedLineIdx]
 
+}
+function SelectLineWithClick(line){
+gMeme.selectedLineIdx = line.id
+}
+
+function DeleteLine(){
+    const lineIdx = gMeme.lines.findIndex(line => line.id === gMeme.selectedLineIdx)
+    gMeme.lines.splice(lineIdx, 1)
+}
+
+function AddLine(){
+    if(!gMeme.lines.length === 2)return
+    gMeme.lines=[
+        {
+            txt:'text',
+            size: 20,
+            color: 'black',
+            pos:{x:225,y:60,rate :20},
+            id:0
+        }, {
+            txt:'text',
+            size: 20,
+            color: 'black',
+            pos:{x:225,y:380,rate :20},
+            id:1
+           
+        }
+    ]
+}
+
+
+function isTextClick(clickedPos) {
+    
+    const clickText = gMeme.lines.find(line => { 
+        const textWidth = gCtx.measureText(line.txt).width * 1.1
+        const lineHeight = line.size * 1.5
+        const { x, y} = line.pos
+        return clickedPos.x >= (x - textWidth / 2) && clickedPos.x <= x + textWidth &&
+                clickedPos.y >= (line.pos.y - lineHeight / 2) && clickedPos.y <= y + lineHeight
+    })
+	
+	return clickText
 }
