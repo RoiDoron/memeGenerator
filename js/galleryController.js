6a0da92f594ee9a677f6d89a10a37392f6789d06
Roi Doron <roidoron6@gmail.com>
2024-02-29 12:32:00 +0200

    Add gallery css

--- js/galleryController.js
+++ js/galleryController.js
@@ -2,8 +2,14 @@
 
 function renderGallery() {
     const elGallery = document.querySelector('.gallery-container')
+    var HTMLstr =''
+for (let i = 1; i < 19; i++) {
+  
+   HTMLstr += `<div class="img-card"><img id="${i}" src="img/${i}.jpg" onclick="onImgSelect(this)"></div>`
+  
+}
+console.log(HTMLstr);
 
-    const HTMLstr = `<div class="img-card"><img id="4" src="img/4.jpg" onclick="onImgSelect(this)"></div><div class="img-card" ><img id="3" src="img/3.jpg" onclick="onImgSelect(this)"></div>`
 
     elGallery.innerHTML = HTMLstr
 }
@@ -13,4 +19,11 @@ const elGallery = document.querySelector('.gallery-container')
 elGallery.hidden=true
   setImg(img)
   renderMeme()
+}
+
+function onGallery(){
+  const elGallery = document.querySelector('.gallery-container')
+  elGallery.hidden=false
+  const elEditor = document.querySelector('.editor-container')
+  elEditor.hidden=true
 }
